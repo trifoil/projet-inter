f5206b47c9715b4920cc49e6429ae57172073719
Mat_squat <diletomatteo@gmail.com>
2024-12-19 13:06:51 +0100

    Give up button option

--- php/file_php/home.php
+++ php/file_php/home.php
@@ -48,7 +48,7 @@
     	        ?>
                     <ul>
                         <?php
-                            echo '<li>',$pe['Location'],' : ', $pe['AvailablePlaces'], ' places restantes sur ',$pe['TotalPlace'],  '<button>+</button></li>';}
+                            echo '<li>',$pe['Location'],' : ', $pe['AvailablePlaces'], ' places restantes sur ',$pe['TotalPlace'];}
                         ?>
                     </ul>
                     <h3>The different car parks in SmartCity Disable:</h3>
