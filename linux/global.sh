9c399e92a58b047689d5272294dd59d191dea9e1
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2024-12-19 15:38:18 +0100

    merde

--- linux/global.sh
+++ linux/global.sh
@@ -80,10 +80,10 @@ FLUSH PRIVILEGES;
 EOF
 
 # Run Django migrations
-python3 manage.py migrate
+python3 projinter/manage.py migrate
 
 # Create a superuser
-python3 manage.py createsuperuser
+python3 projinter/manage.py createsuperuser
 
 sudo firewall-cmd --add-port=8000/tcp --permanent
 sudo firewall-cmd --reload
@@ -99,72 +99,72 @@ echo "Updated ALLOWED_HOSTS in $SETTINGS_FILE"
 
 
 # Run the development server (sent to background)
-nohup python3 manage.py runserver 0.0.0.0:8000 &
+nohup python3 projinter/manage.py runserver 0.0.0.0:8000 &
 
 echo "Django development server is running at http://localhost:8000"
 
 
 
-# Installing samba share
-    echo "Installing Samba share"
+# # Installing samba share
+#     echo "Installing Samba share"
     
-    dnf update -y
-    dnf -y install samba samba-client
-    systemctl enable smb --now
-    systemctl enable nmb --now    
+#     dnf update -y
+#     dnf -y install samba samba-client
+#     systemctl enable smb --now
+#     systemctl enable nmb --now    
 
-    firewall-cmd --permanent --add-service=samba
-    firewall-cmd --reload
+#     firewall-cmd --permanent --add-service=samba
+#     firewall-cmd --reload
 
-    chown -R nobody:nobody /home/admin/projet-inter/linux/
-    chmod -R 0777 /home/admin/projet-inter/linux/
+#     chown -R nobody:nobody /home/admin/projet-inter/linux/
+#     chmod -R 0777 /home/admin/projet-inter/linux/
     
-    cat <<EOL > /etc/samba/smb.unauth.conf
-[unauth_share]
-   path = /home/admin/projet-inter/linux/
-   browsable = yes
-   writable = yes
-   guest ok = yes
-   guest only = yes
-   force user = nobody
-   force group = nobody
-   create mask = 0777
-   directory mask = 0777
-   read only = no
-EOL
+#     cat <<EOL > /etc/samba/smb.unauth.conf
+# [unauth_share]
+#    path = /home/admin/projet-inter/linux/
+#    browsable = yes
+#    writable = yes
+#    guest ok = yes
+#    guest only = yes
+#    force user = nobody
+#    force group = nobody
+#    create mask = 0777
+#    directory mask = 0777
+#    read only = no
+# EOL
     
-    PRIMARY_CONF="/etc/samba/smb.conf"
-    INCLUDE_LINE="include = /etc/samba/smb.unauth.conf"
+#     PRIMARY_CONF="/etc/samba/smb.conf"
+#     INCLUDE_LINE="include = /etc/samba/smb.unauth.conf"
 
-    if ! grep -Fxq "$INCLUDE_LINE" "$PRIMARY_CONF"; then
-        echo "$INCLUDE_LINE" >> "$PRIMARY_CONF"
-        echo "Include line added to $PRIMARY_CONF"
-    else
-        echo "Include line already exists in $PRIMARY_CONF"
-    fi
+#     if ! grep -Fxq "$INCLUDE_LINE" "$PRIMARY_CONF"; then
+#         echo "$INCLUDE_LINE" >> "$PRIMARY_CONF"
+#         echo "Include line added to $PRIMARY_CONF"
+#     else
+#         echo "Include line already exists in $PRIMARY_CONF"
+#     fi
 
-    # SELINUX RAHHHHHHHHHHH
-    /sbin/restorecon -R -v /home/admin/projet-inter/linux/    setsebool -P samba_export_all_rw 1
+#     # SELINUX RAHHHHHHHHHHH
+#     /sbin/restorecon -R -v /home/admin/projet-inter/linux/    setsebool -P samba_export_all_rw 1
 
-setsebool -P httpd_can_network_connect 1
+# setsebool -P httpd_can_network_connect 1
 
-setsebool -P httpd_graceful_shutdown 1
+# setsebool -P httpd_graceful_shutdown 1
 
-setsebool -P httpd_can_network_relay 1
+# setsebool -P httpd_can_network_relay 1
 
-setsebool -P nis_enabled 1
+# setsebool -P nis_enabled 1
 
-setsebool -P samba_export_all_ro 1
+# setsebool -P samba_export_all_ro 1
 
-setsebool -P samba_export_all_rw 1
+# setsebool -P samba_export_all_rw 1
 
-ausearch -c 'php-fpm' --raw | audit2allow -M my-phpfpm
-semodule -X 300 -i my-phpfpm.pp
+# ausearch -c 'php-fpm' --raw | audit2allow -M my-phpfpm
+# semodule -X 300 -i my-phpfpm.pp
 
-    systemctl restart smb
-    systemctl restart nmb
+#     systemctl restart smb
+#     systemctl restart nmb
 
-    echo "Samba services restarted"
+#     echo "Samba services restarted"
 
-    echo "Press any key to continue..."
-    read -n 1 -s key
+#     echo "Press any key to continue..."
+#     read -n 1 -s key
